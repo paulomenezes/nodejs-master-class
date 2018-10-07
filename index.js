62167a6530928ac27624fb07583d313e1c71e3e1
Paulo Menezes <paulo.hgmenezes@gmail.com>
2018-10-07 17:18:19 -0300

    Restful API

---
+++ index.js
@@ -0,0 +1,32 @@
+/**
+ * Primary file for the API
+ */
+
+// Dependencies
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
+
+const server = require('./server');
+const config = require('./config');
+
+// Instantiate the HTTP server
+const httpServer = http.createServer(server);
+
+// Start the HTTP server
+httpServer.listen(config.httpPort, () => {
+  console.log(`The http server is listening on port ${config.httpPort} in ${config.envName} mode`);
+});
+
+// Instantiate the HTTPS server
+const httpsServerOptions = {
+  key: fs.readFileSync('./https/key.pem'),
+  cert: fs.readFileSync('./https/cert.pem'),
+};
+
+const httpsServer = https.createServer(httpsServerOptions, server);
+
+// Start the HTTPS server
+httpsServer.listen(config.httpsPort, () => {
+  console.log(`The https server is listening on port ${config.httpsPort} in ${config.envName} mode`);
+});
---
+++ server.js
@@ -0,0 +1,82 @@
+const url = require('url');
+const StringDecoder = require('string_decoder').StringDecoder;
+
+// All the server logic for both http and https server
+const server = (req, res) => {
+  // Get the URL and parse it
+  const parsedUrl = url.parse(req.url, true);
+
+  // Get the path
+  const path = parsedUrl.pathname;
+  const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+  // Get the query string as an object
+  const queryStringObject = parsedUrl.query;
+
+  // Get the HTTP Method
+  const method = req.method.toUpperCase();
+
+  // Get the headers as an object
+  const headers = req.headers;
+
+  // Get the payloads, if any
+  const decoder = new StringDecoder('UTF-8');
+  let buffer = '';
+
+  req.on('data', data => {
+    buffer += decoder.write(data);
+  });
+
+  req.on('end', () => {
+    buffer += decoder.end();
+
+    // Choose the handler this request should go to
+    // If one is not found, use the Not found handler
+    const chosenHandler = typeof router[trimmedPath] !== 'undefined' ? router[trimmedPath] : handlers.notFound;
+
+    // Construct the data object to send to the handler
+    const data = {
+      trimmedPath,
+      queryStringObject,
+      method,
+      headers,
+      payload: buffer,
+    };
+
+    // Route the request to the handler specified in the router
+    chosenHandler(data, (statusCode, payload) => {
+      // Use the status code called back by the handler, or default to 2000
+      statusCode = typeof statusCode === 'number' ? statusCode : 200;
+
+      // Use the payload called back by the handler, or default to empty object
+      payload = typeof payload === 'object' ? payload : {};
+
+      // Convert the payload to a string
+      const payloadString = JSON.stringify(payload);
+
+      // Return the response
+      res.setHeader('Content-Type', 'application/json');
+      res.writeHead(statusCode);
+      res.end(payloadString);
+
+      // Log the request path
+      console.log(`${method}: ${trimmedPath} - ${statusCode}`);
+    });
+  });
+};
+
+const handlers = {
+  ping: (data, callback) => {
+    callback(200);
+  },
+  notFound: (data, callback) => {
+    callback(404);
+  },
+};
+
+// Define a request router
+const router = {
+  ping: handlers.ping,
+};
+
+module.exports = server;
